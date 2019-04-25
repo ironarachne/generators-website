a695b65f7be25fc96ed07e909a9b1fd003459d53
Ben Overmyer <ben.overmyer@protolabs.com>
2019-04-25 12:59:25 -0500

    Convert environment to use world API

--- app/RegionGenerator.php
+++ app/RegionGenerator.php
@@ -7,7 +7,7 @@ use \GuzzleHttp\Client;
 class RegionGenerator {
     public function generate($id) {
         $client = new Client();
-        $response = $client->request('GET', 'http://' . env('REGIONGENAPI') . '/' . $id);
+        $response = $client->request('GET', 'http://' . env('WORLDAPI') . '/region/' . $id);
         $region = json_decode($response->getBody()->getContents());
 
         return $region;
