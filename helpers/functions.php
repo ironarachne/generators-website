448fe25df66cb70a0792a3219a2102485ac679d3
Ben Overmyer <ben@overmyer.net>
2020-07-10 07:20:04 -0500

    Add seeder function

--- helpers/functions.php
+++ helpers/functions.php
@@ -1,29 +1,50 @@
 <?php
 
-if ( !function_exists( 'distribute_array' ) ) {
-    function distribute_array( $array1, $array2 )
+if (! function_exists('distribute_array')) {
+    function distribute_array($array1, $array2)
     {
         $availableElements = $array2;
 
         $iterations[] = $availableElements;
 
-        foreach ( $array1 as $element ) {
-            $index = rnd( count( $availableElements ) - 1 );
-            $myElement = $availableElements[ $index ];
-            unset( $availableElements[ $index ] );
-            $availableElements = array_values( $availableElements );
+        foreach ($array1 as $element) {
+            $index = rnd(count($availableElements) - 1);
+            $myElement = $availableElements[$index];
+            unset($availableElements[$index]);
+            $availableElements = array_values($availableElements);
 
-            $randomizedElements[ $element ] = $myElement;
+            $randomizedElements[$element] = $myElement;
         }
 
         return $randomizedElements;
     }
 }
 
-if ( !function_exists( 'rnd' ) ) {
-    function rnd( $max )
+if (! function_exists('rnd')) {
+    function rnd($max)
     {
         srand();
-        return mt_rand( 0, $max );
+
+        return mt_rand(0, $max);
+    }
+}
+
+if (! function_exists('seeder')) {
+    function seeder($seed)
+    {
+        $seedNumber = '';
+        $byteArray = unpack('C*', $seed);
+        foreach($byteArray as $b) {
+            $seedNumber .= $b;
+        }
+        $seedNumber = intval($seedNumber);
+        srand($seedNumber);
+    }
+}
+
+if (! function_exists('random_direction')) {
+    function random_direction()
+    {
+        return mt_rand(8);
     }
 }
