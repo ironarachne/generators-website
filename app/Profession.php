1e42879f6e2a01799157ed3917355277892e1ac7
Ben Overmyer <ben@overmyer.net>
2020-08-21 08:32:00 -0500

    Make profession loading work correctly

--- app/Profession.php
+++ app/Profession.php
@@ -9,38 +9,63 @@ use Illuminate\Support\Facades\Cache;
 
 class Profession
 {
-    public $name;
-    public $description;
-    public $tags;
+    public string $name;
+    public string $description;
+    public array $tags;
 
-    public function load($tag)
+    public static function fromJSON($json)
+    {
+        $professions = [];
+
+        $data = json_decode($json);
+
+        foreach ($data->professions as $d) {
+            $p = new Profession();
+
+            $p->name = $d->name;
+            $p->description = $d->description;
+            $p->tags = [];
+
+            foreach ($d->tags as $t) {
+                $tag = new Tag();
+                $tag->name = $t->name;
+                $p->tags [] = $tag;
+            }
+
+            $professions [] = $p;
+        }
+
+        return $professions;
+    }
+
+    public static function load($tag)
     {
         return Cache::remember("professions_$tag", 600, function () use ($tag) {
-            $professions = [];
-
             $url = env('DATA_CORE_URL');
 
             $client = new Client();
             $response = $client->request('GET', $url . '/professions?tag=' . $tag);
 
-            $data = json_decode($response->getBody()->getContents());
-
-            foreach ($data->professions as $d) {
-                $p = new Profession();
-
-                $p->name = $d->name;
-                $p->description = $d->description;
-
-                foreach ($d->tags as $t) {
-                    $tag = new Tag();
-                    $tag->name = $t->name;
-                    $p->tags [] = $tag;
-                }
-
-                $professions [] = $p;
-            }
-
-            return $professions;
+            return Profession::fromJSON($response->getBody()->getContents());
         });
+    }
+
+    public static function loadAll()
+    {
+        return Cache::remember('professions_all', 600, function () {
+            $url = env('DATA_CORE_URL');
+
+            $client = new Client();
+            $response = $client->request('GET', $url . '/professions');
+
+            return Profession::fromJSON($response->getBody()->getContents());
+        });
+    }
+
+    public static function random(): Profession
+    {
+        $professions = Profession::loadAll();
+
+        return random_item($professions);
     }
 }
