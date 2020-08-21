8188f97d17eb926664be502a80c9eab7d5193ea2
Ben Overmyer <ben@overmyer.net>
2020-08-21 08:31:11 -0500

    Add height and weight methods

--- app/AgeCategory.php
+++ app/AgeCategory.php
@@ -6,15 +6,48 @@ namespace App;
 
 class AgeCategory
 {
-    public $name;
-    public $age_max;
-    public $age_min;
-    public $size_category;
-    public $height_base_female;
-    public $height_base_male;
-    public $height_range_dice;
-    public $weight_base_female;
-    public $weight_base_male;
-    public $weight_range_dice;
-    public $commonality;
+    public string $name;
+    public int $age_max;
+    public int $age_min;
+    public string $size_category;
+    public int $height_base_female;
+    public int $height_base_male;
+    public string $height_range_dice;
+    public int $weight_base_female;
+    public int $weight_base_male;
+    public string $weight_range_dice;
+    public int $commonality;
+
+    public function randomAge()
+    {
+        return mt_rand($this->age_min, $this->age_max);
+    }
+
+    public function randomHeight($gender)
+    {
+        $dice = new Dice($this->height_range_dice);
+        $height = $dice->roll();
+
+        if ($gender == 'male') {
+            $height += $this->height_base_male;
+        } else {
+            $height += $this->height_base_female;
+        }
+
+        return $height;
+    }
+
+    public function randomWeight($gender)
+    {
+        $dice = new Dice($this->weight_range_dice);
+        $weight = $dice->roll();
+
+        if ($gender == 'male') {
+            $weight += $this->weight_base_male;
+        } else {
+            $weight += $this->weight_base_female;
+        }
+
+        return $weight;
+    }
 }
